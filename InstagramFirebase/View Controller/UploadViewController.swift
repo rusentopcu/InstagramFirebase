15c82dabb7f7c5db6030a9ec55e1592bb976cfcd
Rusen Topcu <rusentopcu@msn.com>
2020-03-02 15:47:34 +0300

    Save the picture to disk

--- InstagramFirebase/View Controller/UploadViewController.swift
+++ InstagramFirebase/View Controller/UploadViewController.swift
@@ -7,12 +7,16 @@
 //
 
 import UIKit
+import Firebase
 
 class UploadViewController: UIViewController, UIImagePickerControllerDelegate,UINavigationControllerDelegate {
 
     
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var textComment: UITextField!
+    @IBOutlet weak var outletButton: UIButton!
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -34,6 +38,34 @@ class UploadViewController: UIViewController, UIImagePickerControllerDelegate,UI
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         imageView.image = info[.originalImage] as? UIImage
         self.dismiss(animated: true, completion: nil)
+    }
+    
+    
+    @IBAction func actionButtonClicked(_ sender: Any) {
+        
+        let storage = Storage.storage() //Storage sınıfından, storage instance oluşturuldu.
+        let storageReferans = storage.reference() //Storage alanı için ref oluşturuldu
+        let mediaFolder = storageReferans.child("media") //Kayıt edilecek klasör ref
+        
+        if let data = imageView.image?.jpegData(compressionQuality: 0.5) {
+            
+            let imageReferance = mediaFolder.child("image.jpg")
+            imageReferance.putData(data, metadata: nil) { (metadata, error) in
+                if error != nil {
+                    print(error?.localizedDescription)
+                }
+                else {
+                    imageReferance.downloadURL { (url, error) in
+                        if error == nil {
+                            let imageUrl = url?.absoluteString
+                            print(imageUrl)
+                        }
+                    }
+                }
+            }
+            
+        }
+        
     }
     
     
