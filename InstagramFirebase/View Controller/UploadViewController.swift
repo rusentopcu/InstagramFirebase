61722a09721ca2400c00dad8e53ada5818da8ad9
Rusen Topcu <rusentopcu@msn.com>
2020-03-02 13:52:57 +0300

    Choose A Photo on Upload VC

--- InstagramFirebase/View Controller/UploadViewController.swift
+++ InstagramFirebase/View Controller/UploadViewController.swift
@@ -8,13 +8,33 @@
 
 import UIKit
 
-class UploadViewController: UIViewController {
+class UploadViewController: UIViewController, UIImagePickerControllerDelegate,UINavigationControllerDelegate {
 
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var textComment: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-       
+        imageView.isUserInteractionEnabled = true
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(selectImage))
+        imageView.addGestureRecognizer(gestureRecognizer)
         
     }
 
+    @objc func selectImage() {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.sourceType = .photoLibrary
+        picker.allowsEditing = true
+        present(picker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        imageView.image = info[.originalImage] as? UIImage
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    
 }
