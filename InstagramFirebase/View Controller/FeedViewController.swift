87744435c53a90dfb5ca37527c83af519e6ee119
Rusen Topcu <rusentopcu@msn.com>
2020-03-09 10:53:15 +0300

    get data from firestore

--- InstagramFirebase/View Controller/FeedViewController.swift
+++ InstagramFirebase/View Controller/FeedViewController.swift
@@ -7,9 +7,15 @@
 //
 
 import UIKit
+import Firebase
 
 class FeedViewController: UIViewController, UITableViewDelegate,UITableViewDataSource {
 
+    
+    var userEmailArray = [String]()
+    var userCommentArray = [String]()
+    var likeArray = [Int]()
+    var userImageArray = [String]()
     
 
     @IBOutlet weak var tableView: UITableView!
@@ -22,6 +28,8 @@ class FeedViewController: UIViewController, UITableViewDelegate,UITableViewDataS
         tableView.dataSource = self
         
      
+        getDataFromFirestore()
+        
     }
     
         func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -30,10 +38,46 @@ class FeedViewController: UIViewController, UITableViewDelegate,UITableViewDataS
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! FeedCell
-        cell.userEmailLabel.text = "user@mail.com"
-        cell.commentLabel.text = "comment"
-        cell.likeLabel.text = "0"
+        cell.userEmailLabel.text = userEmailArray[indexPath.row]
+        cell.commentLabel.text = userCommentArray[indexPath.row]
+        cell.likeLabel.text = String(likeArray[indexPath.row])
         cell.userImageView.image = UIImage(named: "select.png")
         return cell
+    }
+    
+    
+    func getDataFromFirestore() {
+        
+        let firestoreDatabase = Firestore.firestore()
+        firestoreDatabase.collection("Posts").addSnapshotListener { (snapshot, error) in
+            if error != nil {
+                print(error?.localizedDescription)
+            } else {
+                
+                if snapshot?.isEmpty != true && snapshot != nil {
+                    for document in snapshot!.documents {
+                        let documentID = document.documentID
+                        //print(documentID)
+                        
+                        if let postedBy = document.get("postedBy") as? String {
+                            self.userEmailArray.append(postedBy)
+                        }
+                        
+                        if let postComment = document.get("postComment") as? String {
+                            self.userCommentArray.append(postComment)
+                        }
+                        
+                        if let likes = document.get("likes") as? Int {
+                            self.likeArray.append(likes)
+                        }
+                        
+                        if let imageUrl = document.get("imageUrl") as? String {
+                            self.userImageArray.append(imageUrl)
+                        }
+                    }
+                    self.tableView.reloadData()
+                }
+            }
+        }
     }
 }
